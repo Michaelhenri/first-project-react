9fe752df07d0b0be056cb0755e2dbf0e951259aa
Michaelhenri <michaeldt2015@gmail.com>
2024-06-24 11:33:24 -0300

    add função deletar usuarios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import People from './assets/people.svg'
 import Arrow from './assets/arrow.svg'
 import Trash from './assets/trash.svg'
 
-import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, User } from './styles'
+import {
+  Container,
+  Image,
+  ContainerItens,
+  H1,
+  InputLabel,
+  Input,
+  Button,
+  User
+} from './styles'
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -13,7 +22,19 @@ const App = () => {
 
 
   function addNewUser() {
-    setUsers([...users, { id: Math.random(), name: inputName.current.value, age: inputAge.current.value }])
+    setUsers([...users,
+    {
+      id: Math.random(),
+      name: inputName.current.value,
+      age: inputAge.current.value
+    }]
+    )
+  }
+
+  function deleteUser(userId){
+    const newUsers = users.filter((user) => user.id !== userId);
+
+    setUsers(newUsers)
   }
 
   return (
@@ -36,7 +57,9 @@ const App = () => {
           {users.map((user) => (
             <User key={user.id}>
               <p>{user.name}</p> <p>{user.age}</p>
-              <button><img src={Trash} alt='lata-de-lixo' /></button>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt='lata-de-lixo' />
+              </button>
             </User>
           ))}
         </ul>
