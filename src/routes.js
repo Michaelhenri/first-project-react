e0b1d8bd30a6c48189efbb1bc3afe17941192c2d
Michaelhenri <michaeldt2015@gmail.com>
2024-06-26 15:37:04 -0300

    Criando nova página e as rotas para ela

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes as MyRoutes, Switch, Route } from 'react-router-dom';
+
+import Home from "./containers/Home";
+import Users from "./containers/Users";
+
+function Routes() {
+    return (
+        <Router>
+            <MyRoutes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/usuarios" element={<Users />} />
+            </MyRoutes>
+        </Router>
+    );
+}
+
+export default Routes;
