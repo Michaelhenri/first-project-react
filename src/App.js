14a4c07e3f6b333a4988da9efee87446a2551bde
Michaelhenri <michaeldt2015@gmail.com>
2024-06-26 12:47:55 -0300

    add get e delete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import axios from 'axios'
 
@@ -22,61 +22,69 @@ const App = () => {
   const inputName = useRef()
   const inputAge = useRef()
 
-
+// Add usuarios
   async function addNewUser() {
 
-    const data = await axios.post("http://localhost:3001/users", {
+    const { data: newUser } = await axios.post("http://localhost:3001/users", {
       name: inputName.current.value,
       age: inputAge.current.value,
     });
 
-    console.log(data)
+    setUsers([...users, newUser])
 
-    //   setUsers([...users,
-    //   {
-    //     id: Math.random(),
-    //     name: inputName.current.value,
-    //     age: inputAge.current.value
-    //   }]
-    //   )
-    }
-
-    function deleteUser(userId) {
-      const newUsers = users.filter((user) => user.id !== userId);
-
-      setUsers(newUsers)
-    }
-
-    return (
-      <Container>
-        <Image alt='logo-image' src={People} />
-        <ContainerItens>
-          <H1>DevUsers</H1>
-
-          <InputLabel>Nome</InputLabel>
-          <Input ref={inputName} placeholder='Nome' />
-
-          <InputLabel>Idade</InputLabel>
-          <Input ref={inputAge} placeholder='Idade' />
-
-          <Button onClick={addNewUser}>
-            Cadastrar <img alt='img-seta' src={Arrow} />
-          </Button>
-
-          <ul>
-            {users.map((user) => (
-              <User key={user.id}>
-                <p>{user.name}</p> <p>{user.age}</p>
-                <button onClick={() => deleteUser(user.id)}>
-                  <img src={Trash} alt='lata-de-lixo' />
-                </button>
-              </User>
-            ))}
-          </ul>
-
-        </ContainerItens>
-      </Container>
-    )
   }
 
-  export default App
+ //Guarda usuários 
+  useEffect(() => {
+
+    async function fetchUsers() {
+      const { data: newUsers } = await axios.get("http://localhost:3001/users");
+
+      setUsers(newUsers);
+    }
+
+    fetchUsers()
+  }, [])
+
+ //Deleta usuários 
+  async function deleteUser(userId) {
+    await axios.delete(`http://localhost:3001/users/${userId}`)
+
+    const newUsers = users.filter((user) => user.id !== userId);
+
+    setUsers(newUsers)
+  }
+
+  return (
+    <Container>
+      <Image alt='logo-image' src={People} />
+      <ContainerItens>
+        <H1>DevUsers</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder='Nome' />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder='Idade' />
+
+        <Button onClick={addNewUser}>
+          Cadastrar <img alt='img-seta' src={Arrow} />
+        </Button>
+
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt='lata-de-lixo' />
+              </button>
+            </User>
+          ))}
+        </ul>
+
+      </ContainerItens>
+    </Container>
+  )
+}
+
+export default App
